5d28058c71b3b0a863b4d5399ad534127e8090d9
lucas-fgsake <lucas@fgsake.org>
2025-12-28 19:40:31 -0600

    update presentation tool resolver to new format

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,46 +2,10 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {schemaTypes} from './schemas'
-import {presentationTool, DocumentLocationResolver} from 'sanity/presentation'
-import {Observable, map} from 'rxjs'
+import {defineDocuments, defineLocations, presentationTool} from 'sanity/presentation'
 
 export const projectId = process.env.SANITY_STUDIO_PROJECT_ID!
 export const dataset = process.env.SANITY_STUDIO_DATASET!
-
-const locate: DocumentLocationResolver = (params, context) => {
-  const {documentStore} = context
-
-  if (params.type === 'post') {
-    // Listen to the query and fetch the draft and published document
-    const doc$ = documentStore.listenQuery(`*[_id == $id][0]{slug,title}`, params, {
-      perspective: 'drafts',
-    }) as Observable<{
-      slug: {current: string | null} | null
-      title: string | null
-    } | null>
-
-    return doc$.pipe(
-      map((doc) => {
-        if (!doc || !doc.slug?.current) return null
-
-        return {
-          locations: [
-            {
-              title: doc.title || 'Untitled',
-              href: `/post/${doc.slug.current}`,
-            },
-            {
-              title: 'Posts',
-              href: `/`,
-            },
-          ],
-        }
-      }),
-    )
-  }
-
-  return null
-}
 
 export default defineConfig({
   name: 'project-name',
@@ -52,7 +16,25 @@ export default defineConfig({
     structureTool(),
     presentationTool({
       previewUrl: process.env.SANITY_STUDIO_PREVIEW_URL || 'http://localhost:3000',
-      locate,
+      resolve: {
+        mainDocuments: defineDocuments([
+          {
+            route: '/post/:slug',
+            filter: `_type == "post" && slug.current == $slug`,
+          },
+        ]),
+        locations: {
+          post: defineLocations({
+            select: {title: 'title', slug: 'slug.current'},
+            resolve: (doc) => ({
+              locations: [
+                {title: doc?.title, href: `/post/${doc?.slug}`},
+                {title: 'My Remix Sanity Homepage', href: `/`},
+              ],
+            }),
+          }),
+        },
+      },
     }),
     visionTool(),
   ],
